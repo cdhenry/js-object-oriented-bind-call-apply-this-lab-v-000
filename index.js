751cc38332b1282a50ae06f5dbf1b27499098697
Chris Henry <cdhenry@gmail.com>
2018-04-22 17:28:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function setThisWithCall(fn, thisValue, arg){
 
 function setThisWithApply(fn, thisValue, args){
   debugger;
-  return fn.call(thisValue, [args]);
+  return fn.call(thisValue, args);
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue){
