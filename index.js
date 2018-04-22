28164ad0d3cee9c4df9c8f68db69ca3c0f844501
Chris Henry <cdhenry@gmail.com>
2018-04-22 17:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function justInvoke(fn){
 
 function setThisWithCall(fn, thisValue, arg){
   debugger;
-  return fn.call(arg) = thisValue;
+  return fn.call(thisValue) = arg;
 }
 
 function setThisWithApply(fn, thisValue, args){
